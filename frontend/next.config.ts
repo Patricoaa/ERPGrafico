c53c534b4dc832f62e54aff18cfe450d012f9f9c
AdminImprenta <patoxlp13@gmail.com>
2026-01-02 16:49:58 -0300

    feat: add Next.js configuration to transpile `react-day-picker`

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["react-day-picker"],
 };
 
 export default nextConfig;
